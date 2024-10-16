17348302ff1a557726b5fcd28807a0bf3880e39a
JoelFaldin <joelfaldin@gmail.com>
2024-10-15 21:02:49 -0300

    feat: adding tests for directions service

--- src/directions/__tests__/directions.controller.spec.ts
+++ src/directions/__tests__/directions.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { DirectionsController } from '../directions.controller';
+import { PrismaService } from '../../prisma/prisma.service';
+import { DirectionsService } from '../directions.service';
 
 describe('DirectionsController', () => {
   let controller: DirectionsController;
@@ -7,6 +10,7 @@ describe('DirectionsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DirectionsController],
+      providers: [PrismaService, DirectionsService],
     }).compile();
 
     controller = module.get<DirectionsController>(DirectionsController);
