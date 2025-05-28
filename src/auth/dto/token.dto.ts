4e6d6f84c53cec29f9c42a4c0a80254ae30af4de
JoelF <joelfaldin@gmail.com>
2025-05-28 13:59:18 -0400

    feat: adding SpringBoot project!

--- src/auth/dto/token.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class tokenDTO {
-  @IsString()
-  @IsNotEmpty()
-  token: string;
-}
---
+++ src/main/java/com/joelf/connect_desk_backend/ConnectDeskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joelf.connect_desk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectDeskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectDeskBackendApplication.class, args);
+	}
+
+}
