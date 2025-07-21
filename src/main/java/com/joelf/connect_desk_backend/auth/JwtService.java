b43723eed27daa9e2719697ac050b1779badab0d
JoelF <joelfaldin@gmail.com>
2025-07-21 01:06:08 -0400

    fix: fixing auth secret problem and cors problem

--- src/main/java/com/joelf/connect_desk_backend/auth/JwtService.java
+++ src/main/java/com/joelf/connect_desk_backend/auth/JwtService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,20 +18,27 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 
-  public final String jwtSecret = "secret :D";
+  @Value("${jwt.secret}")
+  public String jwtSecret;
 
   public String generateToken(User user) {
+    SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret));
+
+    System.out.println("SIGN KEY: " + Base64.getEncoder().encodeToString(key.getEncoded()));
+
     return Jwts.builder()
         .subject(user.getEmail())
         .issuedAt(new Date(System.currentTimeMillis()))
         .expiration(new Date(System.currentTimeMillis() + 86400000))
-        .signWith(Jwts.SIG.HS256.key().build())
+        .signWith(key)
         .compact();
   }
 
   public String extractUserName(String token) {
     SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret));
     JwtParser parser = Jwts.parser().verifyWith(key).build();
+
+    System.out.println("SIGN KEY: " + Base64.getEncoder().encodeToString(key.getEncoded()));
 
     return parser.parseSignedClaims(token).getPayload().getSubject();
   }
