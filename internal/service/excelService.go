9c5a830d908038e3db63f9171325db3cb353c4cc
JoelFaldin <joelfaldin@gmail.com>
2026-05-18 23:20:35 -0400

    refactor: improve header style on template

--- internal/service/excelService.go
+++ internal/service/excelService.go
@@ -18,6 +18,7 @@ func (h *ExcelService) GenerateTemplate() (*excelize.File, error) {
 }
 
 func createExcelFile() (*excelize.File, error) {
+	// Configuring file:
 	file := excelize.NewFile()
 	defer func() {
 		if err := file.Close(); err != nil {
@@ -31,19 +32,65 @@ func createExcelFile() (*excelize.File, error) {
 		return nil, err
 	}
 
+	// Set headers:
 	err = file.SetSheetRow("template", "A1", &[]any{"Rut", "Names", "Lastnames", "Email", "Role", "Departments", "Directions", "Job Number", "Contact"})
 	if err != nil {
 		return nil, err
 	}
 
-	err = file.SetColWidth("template", "C", "C", float64(len("Lastnames")+2))
-	err = file.SetColWidth("template", "F", "F", float64(len("Departments")+2))
-	err = file.SetColWidth("template", "G", "G", float64(len("Directions")+2))
-	err = file.SetColWidth("template", "H", "H", float64(len("Job Number")+2))
+	colWidths := []struct {
+		col    string
+		header string
+	}{
+		{"C", "Lastnames"},
+		{"F", "Departments"},
+		{"G", "Directions"},
+		{"H", "Job Number"},
+	}
+
+	for _, cw := range colWidths {
+		if err := file.SetColWidth("template", cw.col, cw.col, float64(len(cw.header)+2)); err != nil {
+			return nil, err
+		}
+	}
+
+	// Style headers:
+	style, err := file.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{Type: "pattern", Color: []string{"#B2B2B2"}, Pattern: 1},
+		Border: []excelize.Border{
+			{Type: "left", Color: "000000", Style: 1},
+			{Type: "top", Color: "000000", Style: 1},
+			{Type: "right", Color: "000000", Style: 1},
+			{Type: "bottom", Color: "000000", Style: 1},
+		},
+	})
 	if err != nil {
 		return nil, err
 	}
 
+	headerCells := []struct {
+		col    string
+		header string
+	}{
+		{"A", "Rut"},
+		{"B", "Names"},
+		{"C", "Lastnames"},
+		{"D", "Email"},
+		{"E", "Role"},
+		{"F", "Departments"},
+		{"G", "Directions"},
+		{"H", "Job Number"},
+		{"I", "Contact"},
+	}
+
+	for _, hc := range headerCells {
+		location := fmt.Sprintf("%s1", hc.col)
+		if err := file.SetCellStyle("template", location, location, style); err != nil {
+			return nil, err
+		}
+	}
+
+	// Preparing file for return:
 	file.SetActiveSheet(index)
 
 	if err := file.SaveAs("template.xlsx"); err != nil {
